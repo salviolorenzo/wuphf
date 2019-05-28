207fe7453e3fd99facbd52cf376f651f3f9c25b9
salviolorenzoWela <salviolorenzo@gmail.com>
2019-05-28 00:03:04 -0400

    adding basic fileas

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.scss';
+
+export default function Header(props) {
+  return (
+    <header>
+      <div className="h_tags">
+        <h1>Wuphf.com</h1>
+        <h4>Fun, Communication, Connection</h4>
+      </div>
+    </header>
+  );
+}
